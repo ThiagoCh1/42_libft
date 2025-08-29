b5af0cfa7ed5d48b106ed94c2c9d0db25dbf4ed9
ThiagoCh1 <thiagoribeirochico@gmail.com>
2025-08-29 06:48:25 +0100

    Add first ft_ file

--- ft_strlen.c
+++ ft_strlen.c
@@ -1,12 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   teste.c                                            :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thribeir <thiago.ribeiro@teste.com>        +#+  +:+       +#+        */
+/*   By: thribeir <thribeir@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/29 05:42:30 by thribeir          #+#    #+#             */
-/*   Updated: 2025/08/29 05:42:30 by thribeir         ###   ########.fr       */
+/*   Created: 2025/08/29 06:09:50 by thribeir          #+#    #+#             */
+/*   Updated: 2025/08/29 06:09:50 by thribeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
+int ft_strlen(char* str)
+{
+    int	count;
+
+    count = 0;
+    while (*str)
+    {
+        *str++;
+        count++;
+    }
+    return (count);
+}
+
+int main(void)
+{
+	int a = ft_strlen("macaco");
+	printf("%d", a);
+}
