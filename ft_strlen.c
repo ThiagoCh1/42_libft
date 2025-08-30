5bcb2f1f7d932c75c00239ed6636f96187b434ae
ThiagoCh1 <thribeir@DESKTOP-KV81D1N.localdomain>
2025-08-30 16:59:25 +0100

    norminette applied

--- ft_strlen.c
+++ ft_strlen.c
@@ -5,28 +5,28 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: thribeir <thribeir@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/29 06:09:50 by thribeir          #+#    #+#             */
-/*   Updated: 2025/08/29 06:09:50 by thribeir         ###   ########.fr       */
+/*   Created: 2025/08/30 16:52:56 by thribeir          #+#    #+#             */
+/*   Updated: 2025/08/30 16:55:08 by thribeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int ft_strlen(char* str)
+int	ft_strlen(char *str)
 {
-    int	count;
+	int	count;
 
-    count = 0;
-    while (*str)
-    {
-        *str++;
-        count++;
-    }
-    return (count);
+	count = 0;
+	while (*str)
+	{
+		count++;
+		*str++;
+	}
+	return (count);
 }
 
-int main(void)
+/*int main(void)
 {
-	int a = ft_strlen("macaco");
-	printf("%d", a);
-}
+	int a = ft_strlen("donkey");
+	printf("%d\n", a);
+}*/
