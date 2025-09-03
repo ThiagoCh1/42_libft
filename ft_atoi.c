343111c4239b4ec47aefbe1d5f82a77fe1a58c7c
ThiagoCh1 <thiagoribeirochico@gmail.com>
2025-09-03 22:18:52 +0100

    strnstr and atoi

---
+++ ft_atoi.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thribeir <thribeir@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/03 21:36:42 by thribeir          #+#    #+#             */
+/*   Updated: 2025/09/03 21:55:43 by thribeir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	sign;
+	int	num;
+
+	i = 0;
+	sign = 1;
+	num = 0;
+	while (str[i] >= 9 && str[i] <= 13 || str[i] == 32)
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		num = (num * 10) + str[i] - '0';
+		i++;
+	}
+	return (num * sign);
+}
+
+/*int main(void)
+{
+	const char *test_str = "  -42abc";
+	int result;
+
+	result = ft_atoi(test_str);
+	printf("ft_atoi(\"%s\") = %d\n", test_str, result);
+
+	return (0);
+}*/
---
+++ ft_strnstr.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thribeir <thribeir@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/03 21:02:35 by thribeir          #+#    #+#             */
+/*   Updated: 2025/09/03 21:36:15 by thribeir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strnstr(const char *str, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	if (!*needle)
+		return ((char *)str);
+	if (!str || !needle)
+		return (NULL);
+	i = 0;
+	while (i < len && str[i])
+	{
+		j = 0;
+		while ((i + j) < len && str[i + j]
+			&& needle[j] && str[i + j] == needle[j])
+		{
+			j++;
+		}
+		if (!needle[j])
+			return ((char *)(str + i));
+		i++;
+	}
+	return (NULL);
+}
+
+/*int main(void)
+{
+	const char *haystack = "Hello, World!";
+	const char *needle = "World";
+	char *result;
+
+	result = ft_strnstr(haystack, needle, 13);
+
+	if (result)
+		printf("Found '%s' at position: %ld\n", needle, result - haystack);
+	else
+		printf("'%s' not found\n", needle);
+
+	return (0);
+}*/
